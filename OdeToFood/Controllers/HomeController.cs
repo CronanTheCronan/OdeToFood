da5a7a89460b5a2a045414b9bad59366cf8e726c
mattcronan.webdev <mattcronan.webdev@gmail.com>
2018-10-27 21:58:43 -0500

    first commit

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+using OdeToFood.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public HomeController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _restaurantData.GetAll();
+
+            return View(model);
+            
+        }
+
+        private IRestaurantData _restaurantData;
+    }
+}
---
+++ OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Hickory Falls" },
+                new Restaurant { Id = 2, Name = "Pizza Hut" },
+                new Restaurant { Id = 2, Name = "Oscar's Taco Shop"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+
+        List<Restaurant> _restaurants;
+    }
+}
